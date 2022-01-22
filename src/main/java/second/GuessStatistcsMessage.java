9957abdf021d42f4dc2233ab8fab683f966b8c04
오성식 <oss0202@naver.com>
2022-01-22 15:29:29 +0900

    의미있는 이름 - 함수 쪼개기

---
+++ src/main/java/second/GuessStatistcsMessage.java
@@ -0,0 +1,47 @@
+package second;
+
+/**
+ * GuessStatistcsMessageNotGood class에 있는
+ * printGuessStatistics 함수를 맥락을 개선해서 함수를 쪼개었다.
+ * 이럴 경우 알고리즘이 좀 더 명확해 진다.
+ */
+public class GuessStatistcsMessage {
+    private String number;
+    private String verb;
+    private String pluraModifier;
+
+    public String make(char candidate, int count){
+        createPlualDependentMessageParts(count);
+        return String.format(
+                "There %s %s %s%s", verb, number, candidate, pluraModifier
+        );
+    }
+
+    private void createPlualDependentMessageParts(int count) {
+        if(count == 0){
+            thereAreNoLetters();
+        }else if(count == 1){
+            thereIsOneLetter();
+        }else{
+            thereAreManyLetters(count);
+        }
+    }
+
+    private void thereAreManyLetters(int count) {
+        number = Integer.toString(count);
+        verb = "are";
+        pluraModifier = "s";
+    }
+
+    private void thereIsOneLetter() {
+        number = "1";
+        verb = "is";
+        pluraModifier = "";
+    }
+
+    private void thereAreNoLetters() {
+        number = "no";
+        verb = "are";
+        pluraModifier = "s";
+    }
+}
---
+++ src/main/java/second/GuessStatistcsMessageNotGood.java
@@ -0,0 +1,32 @@
+package second;
+
+public class GuessStatistcsMessageNotGood {
+    /**
+     * 3개의 String 변수의 의미는 함수를 끝까지 읽어야지만 어떤 변수인지 파악할 수 있다.
+     * 세 변수를 함수 전반에 사용하기 떄문에 작은 조각으로 쪼개야 맥락이 분명해진다.
+     * @param candidate
+     * @param count
+     */
+    private void printGuessStatistics(char candidate, int count){
+        String number;
+        String verb;
+        String pluraModifier;
+        if(count == 0){
+            number = "no";
+            verb = "are";
+            pluraModifier = "s";
+        }else if(count == 1){
+            number = "1";
+            verb = "is";
+            pluraModifier = "";
+        }else {
+            number = Integer.toString(count);
+            verb = "are";
+            pluraModifier = "s";
+        }
+        String guessMessage = String.format(
+                "There %s %s %s%s", verb, number, candidate, pluraModifier
+        );
+        System.out.println(guessMessage);
+    }
+}
