b44e1a5365193ffe0e4a9f4d41e8267b36056dcc
오성식 <oss0202@naver.com>
2022-01-24 16:59:16 +0900

    인수목록 test

---
+++ src/main/java/chapter03/ArgsTest.java
@@ -0,0 +1,16 @@
+package chapter03;
+
+public class ArgsTest {
+
+    private static String returnString = "";
+    public static void main(String[] args) {
+        String argsTest = ArgsTest.concatString("가나다","라마바","사아자");
+        System.out.println(argsTest);
+    }
+    public static String concatString(String ...args){
+        for (int i = 0; i < args.length; i++) {
+            returnString += args[i];
+        }
+        return returnString;
+    }
+}
