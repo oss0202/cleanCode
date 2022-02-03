6983681002471bd1503203e0b7aaf27906ed107c
오성식 <oss0202@naver.com>
2022-02-03 17:56:45 +0900

    assert test

--- src/main/java/chapter03/ArgsTest.java
+++ src/main/java/chapter03/ArgsTest.java
@@ -5,12 +5,14 @@ public class ArgsTest {
     private static String returnString = "";
     public static void main(String[] args) {
         String argsTest = ArgsTest.concatString("가나다","라마바","사아자");
+        argsTest = null;
         System.out.println(argsTest);
     }
     public static String concatString(String ...args){
         for (int i = 0; i < args.length; i++) {
             returnString += args[i];
         }
+        assert false : "args is null";
         return returnString;
     }
 }
